40a7effc961296eea41ce053afce447f2210918a
Dale Bewley <dbewley@redhat.com>
2021-02-13 10:32:21 -0800

    fix call to sed for mac os

--- apps/robot-shop/build.sh
+++ apps/robot-shop/build.sh
@@ -4,5 +4,5 @@ rm manifest.yaml
 for f in manifests/*.yaml; do (cat "${f}"; echo) >> manifest.yaml; done
 
 if [ "$1" == "4" ]; then
-  sed -i "s/v1beta1/v1/g" manifest.yaml
+  sed -i -e "s/v1beta1/v1/g" manifest.yaml
 fi
